f2b66ce10d33e44596ed4257649933757fd82d08
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-10-04 21:04:17 +0530

    Initial commit

---
+++ src/test/java/com/vijaygenius123/learning/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijaygenius123.learning.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
